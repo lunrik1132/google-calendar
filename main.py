7cae00f4ab9637658577280ec9dbf845eca5bc45
lunrik1132 <prvlad007@gmail.com>
2026-04-08 22:39:40 +0300

    fix4

--- main.py
+++ main.py
@@ -131,7 +131,10 @@ async def logout(request: Request):
 
 @app.get("/", response_class=HTMLResponse)
 async def ui(request: Request):
-    user = request.session.get("user")
+    user = {
+        "logged_in": True,
+        "token": request.session.get("token", {}).get("access_token"),
+    }
 
     if not user:
         return RedirectResponse(url="/login")
